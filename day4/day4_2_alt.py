3930404f39ef617b70234cf1a934392d649e1785
BrunoGomesCoelho <bgc5612@nyu.edu>
2022-12-05 13:57:53 -0500

    Add alt for day4

---
+++ day4/day4_2_alt.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+total = 0
+
+for line in stdin:
+    (a, b), (c, d) = [a.split("-") for a in line.strip().split(",")]
+    a, b, c, d = int(a), int(b), int(c), int(d)
+
+    if a <= c <= b or a <= d <= b or c <= a <= d or c <= b <= d:
+        print(f"Overlap: {a}-{b} and {c}-{d}")
+        total += 1
+
+print(total)
