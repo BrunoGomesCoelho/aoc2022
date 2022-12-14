6de12913c0627ff76f99e31e9e76a91fbeea6647
BrunoGomesCoelho <bgc5612@nyu.edu>
2022-12-14 09:25:20 -0500

    Finish day 8

--- day8/day8_2.py
+++ day8/day8_2.py
@@ -2,31 +2,28 @@ from sys import stdin
 import numpy as np
 
 
+def get_visible_height(array, element):
+    count = 0
+
+    for num in array:
+        count +=1
+        if num >= element:
+            break
+
+    return count
+
+
 matrix = np.genfromtxt(stdin, delimiter=1, dtype=int)
-visible = set()
+scenic_score = 0
+
 for i in range(1, matrix.shape[0]-1):
     for j in range(1, matrix.shape[1]-1):
-        # right > left
-        if all(matrix[i,:j] < matrix[i,j]):
-            print(f"{(i, j)} is visible by left")
-            visible.add((i, j))
-        # left > right
-        if all(matrix[i,j] > matrix[i,j+1:]):
-            print(f"{(i, j)} is visible by right")
-            visible.add((i, j))
-        # up > down
-        if all(matrix[:i,j] < matrix[i,j]):
-            print(f"{(i, j)} is visible by up")
-            visible.add((i, j))
-        # down > up
-        if all(matrix[i,j] > matrix[i+1:,j]):
-            print(f"{(i, j)} is visible by down")
-            visible.add((i, j))
+        right_to_left = get_visible_height(matrix[i, :j][::-1], matrix[i, j])
+        left_to_right = get_visible_height(matrix[i, j+1:], matrix[i, j])
+        up_to_down = get_visible_height(matrix[:i, j][::-1], matrix[i, j])
+        down_to_up = get_visible_height(matrix[i+1:, j], matrix[i, j])
 
+        scenic_score = max(scenic_score, right_to_left * left_to_right * up_to_down * down_to_up)
 
-# dont forget to sum edge
-cols = matrix.shape[1]
-rows = matrix.shape[0]
-edges = cols + cols + 2*rows-4
-print(visible, edges)
-print(len(visible) + edges)
+# foda-se edges
+print(scenic_score)
